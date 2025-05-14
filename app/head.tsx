7b6f321a065a5a6ee27a2c0c8c2515cfdaf87d54
CodeBulider <197011288+Lin24xp@users.noreply.github.com>
2025-05-14 18:51:40 -0400

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,20 +1,11 @@
-// app/head.tsx
-export default function Head() {
-  return (
-    <>
-      <title>EndlessChat</title>
-      {/* Google Analytics */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-RSK3XJ3F9F"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RSK3XJ3F9F');
-          `,
-        }}
-      />
-    </>
-  );
-}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RSK3XJ3F9F"></script>
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-RSK3XJ3F9F');
+    `,
+  }}
+></script>
